0c0c1be32c516834baf94734c73c3bbfe7ecf2d9
Tim Jarratt <tjarratt@RetinaBook.local>
2013-04-26 20:47:54 -0700

    Change secret token
    There were no sessions before, so who cares if it changed (it did). This is set
    as an environment variable, which is the correct way to do this if you're going
    to have a public repo.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Caltrain::Application.config.secret_token = '9443e082112ea917c4f986822d8b4e080592cec15e06f00786376b2374938d70387ec5c1340c98670e683101720079cc35bc8bf430c7c5229b1e101cb0ab604e'
+Caltrain::Application.config.secret_token = ENV['SECRET_TOKEN']
